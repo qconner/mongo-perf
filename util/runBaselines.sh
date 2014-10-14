dc39fcbdf15677e6b0579e9f8b60a0a5e02cc0c3
Quentin Conner <quentin@mongodb.com>
2014-10-14 08:43:27 -0500

    newest releases added

--- util/runBaselines.sh
+++ util/runBaselines.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 #
 touch ~/build-perf
-util/benchrun_daemon.sh r2.4.10 || exit 1
-util/benchrun_daemon.sh r2.4.11 || exit 1
-util/benchrun_daemon.sh r2.6.0 || exit 1
-util/benchrun_daemon.sh r2.6.1 || exit 1
-util/benchrun_daemon.sh r2.6.2 || exit 1
-util/benchrun_daemon.sh r2.6.3 || exit 1
-util/benchrun_daemon.sh r2.6.4 || exit 1
-util/benchrun_daemon.sh r2.7.0 || exit 1
-util/benchrun_daemon.sh r2.7.1 || exit 1
-util/benchrun_daemon.sh r2.7.2 || exit 1
-util/benchrun_daemon.sh r2.7.3 || exit 1
-util/benchrun_daemon.sh r2.7.4 || exit 1
-util/benchrun_daemon.sh r2.7.5 || exit 1
+util/benchrun_daemon.sh r2.4.10
+util/benchrun_daemon.sh r2.4.11
+util/benchrun_daemon.sh r2.4.12
+util/benchrun_daemon.sh r2.6.0
+util/benchrun_daemon.sh r2.6.1
+util/benchrun_daemon.sh r2.6.2
+util/benchrun_daemon.sh r2.6.3
+util/benchrun_daemon.sh r2.6.4
+util/benchrun_daemon.sh r2.6.5
+util/benchrun_daemon.sh r2.7.0
+util/benchrun_daemon.sh r2.7.1
+util/benchrun_daemon.sh r2.7.2
+util/benchrun_daemon.sh r2.7.3
+util/benchrun_daemon.sh r2.7.4
+util/benchrun_daemon.sh r2.7.5
+util/benchrun_daemon.sh r2.7.6
+util/benchrun_daemon.sh r2.7.7
 rm -f ~/build-perf
 
