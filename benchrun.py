a17ff2ecc8934de985211b8e85ff1b711d1f2cd6
Quentin Conner <quentin@mongodb.com>
2014-10-15 18:27:51 -0500

    windows compatibility

--- benchrun.py
+++ benchrun.py
@@ -77,7 +77,7 @@ def parse_arguments():
     parser.add_argument('--nodyno', dest='nodyno', action='store_true', help='dont submit test results to dyno')
     parser.add_argument('--suite', dest='suite',
                         help='run just the specified suite e.g. --suite "[\'insert\',\'remove\']"',
-                        default='sanity')
+                        default="\'sanity\'")
 
     return parser
 
@@ -307,7 +307,7 @@ def main():
                   str(args.seconds) + ", " +
                   str(args.trials) + ", " +
                   "'" + args.reportlabel + "', " +
-                  "'" + str(args.suite) + "'" + ", " +
+                  + str(args.suite) + ", " +
                   "'" + args.reporthost + "', " +
                   "'" + args.reportport + "', " +
                   "'" + str(datetime.datetime.now()) + "', " +
